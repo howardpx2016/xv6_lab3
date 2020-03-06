27b48d5ce01692b4e9633837ca7ef73db76ded7e
howardpx2016 <cchen277@sledge.cs.ucr.edu>
2020-03-05 21:39:05 -0800

    Makefile – added lab3.c to UPROG, EXTRAS, memlayout.h – added STACKBASE to replace sz for exec(), exec.c - modified exec.c to move stack to the top

--- exec.c
+++ exec.c
@@ -62,11 +62,12 @@ exec(char *path, char **argv)
 
   // Allocate two pages at the next page boundary.
   // Make the first inaccessible.  Use the second as the user stack.
-  sz = PGROUNDUP(sz);
-  if((sz = allocuvm(pgdir, sz, sz + 2*PGSIZE)) == 0)
+  // Lab3: move stack up to STACKBASE (KERNBASE-1), allocate only 1 page 
+  //sz = PGROUNDUP(sz); not needed for lab3
+  if(allocuvm(pgdir, PGROUNDDOWN(STACKBASE), STACKBASE) == 0) //changed for lab3. original:if sz = allocuvm(pgdir, sz, sz + 2*PGSIZE)
     goto bad;
-  clearpteu(pgdir, (char*)(sz - 2*PGSIZE));
-  sp = sz;
+  //clearpteu(pgdir, (char*)(sz - 2*PGSIZE)); not needed for lab3
+  sp = STACKBASE;
 
   // Push argument strings, prepare rest of stack in ustack.
   for(argc = 0; argv[argc]; argc++) {
